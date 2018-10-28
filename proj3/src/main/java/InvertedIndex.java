4d5f69e5726b23b7837c375e588a2cca12a9fe87
Your Name <you@example.com>
2018-10-28 23:00:16 +0800

    kl

---
+++ proj3/src/main/java/InvertedIndex.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.ByteArrayInputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class InvertedIndex {
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+            job = new Job(conf, "invert index"); 
+            job.setJarByClass(InvertedIndex.class); 
+            job.setInputFormatClass(TextInputFormat.class); 
+            job.setMapperClass(InvertedIndexMapper.class); 
+            job.setReducerClass(InvertedIndexReducer.class); 
+            job.setOutputKeyClass(Text.class); 
+            job.setOutputValueClass(Text.class); 
+            FileInputFormat.addInputPath(job, new Path(args[0])); 
+            FileOutputFormat.setOutputPath(job, new Path(args[1])); 
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
+        } catch (Exception e) { 
+            e.printStackTrace(); 
+        }
+    }
+}
--- proj3/src/main/java/Proj3.java
+++
@@ -1,5 +0,0 @@
-public class Proj3 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
