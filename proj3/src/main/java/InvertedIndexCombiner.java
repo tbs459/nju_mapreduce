d2a4a63b9c098c9512ddbfc44c12031c7ac0e406
Robert Ridley <robert.n.ridley@gmail.com>
2018-10-31 20:34:34 +0800

    add imports

--- proj3/src/main/java/InvertedIndexCombiner.java
+++ proj3/src/main/java/InvertedIndexCombiner.java
@@ -1,4 +1,10 @@
-public static class InvertedIndexCombiner extends Reducer<Text, Text, Text, Text>{
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+
+public class InvertedIndexCombiner extends Reducer<Text, Text, Text, Text>{
         private Text info = new Text();
         @Override
         protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context)
