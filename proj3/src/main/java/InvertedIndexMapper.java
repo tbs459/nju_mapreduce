03e4d3c34cf6f88459838c25e633f57c4cfb4913
Robert Ridley <robert.n.ridley@gmail.com>
2018-10-31 16:24:02 +0800

    IndexMapper to write key-value to contenxt, as: (word, document_name)

--- proj3/src/main/java/InvertedIndexMapper.java
+++ proj3/src/main/java/InvertedIndexMapper.java
@@ -1,18 +1,23 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
+
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
-public class InvertedIndexMapper extends Mapper<Text, Text, Text, Text> { 
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class InvertedIndexMapper extends Mapper<LongWritable, Text, Text, Text> {
     @Override
-    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException{ 
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
         FileSplit fileSplit = (FileSplit)context.getInputSplit();
-        String fileName = fileSplit.getPath().getName();
+        String docName = fileSplit.getPath().getName();
+        Text document = new Text();
+        document.set(docName);
         Text word = new Text();
-        Text fileName_lineOffset = new Text(fileName+"#"+key.toString()); 
         StringTokenizer itr = new StringTokenizer(value.toString());
-        for(; itr.hasMoreTokens(); ){ 
+        while (itr.hasMoreTokens()) {
             word.set(itr.nextToken());
-            context.write(word, fileName_lineOffset); 
+            context.write(word, document);
         }
-    } 
+    }
 }
