d661ad9b418381b8287223001bdbbe357a14a1fe
Robert Ridley <robert.n.ridley@gmail.com>
2018-10-31 20:35:33 +0800

    updates to mapper

--- proj3/src/main/java/InvertedIndexMapper.java
+++ proj3/src/main/java/InvertedIndexMapper.java
@@ -11,13 +11,15 @@ public class InvertedIndexMapper extends Mapper<LongWritable, Text, Text, Text> 
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
         FileSplit fileSplit = (FileSplit)context.getInputSplit();
         String docName = fileSplit.getPath().getName();
-        Text document = new Text();
-        document.set(docName);
-        Text word = new Text();
+        String count = "1";
+        Text newKey = new Text();
+        Text newVal = new Text();
         StringTokenizer itr = new StringTokenizer(value.toString());
         while (itr.hasMoreTokens()) {
-            word.set(itr.nextToken());
-            context.write(word, document);
+            String combinedValue = itr.nextToken() + ":" + docName;
+            newKey.set(combinedValue);
+            newVal.set(count);
+            context.write(newKey, newVal);
         }
     }
 }
