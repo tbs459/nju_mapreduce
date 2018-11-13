4b48a65cad098e2af6489d033a49506b164154a7
Robert Ridley <robert.n.ridley@gmail.com>
2018-11-13 22:57:30 +0800

    code for mapper and reducer of second job.

---
+++ proj4/src/main/java/MidCodeMapper.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+public class MidCodeMapper extends Mapper<LongWritable, Text, Text, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+        StringTokenizer tokenizer = new StringTokenizer(value.toString(), "\n");
+        Text firstID = new Text();
+        Text secondID = new Text();
+        while (tokenizer.hasMoreTokens()){
+            String[] socialIDs = tokenizer.nextToken().split("\\+");
+            firstID.set(socialIDs[0]);
+            secondID.set(socialIDs[1]);
+            context.write(firstID, secondID);
+        }
+    }
+}
---
+++ proj4/src/main/java/MidCodeReducer.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class MidCodeReducer extends Reducer<Text, Text, Text, Text> {
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        ArrayList<String> listOfConnections = new ArrayList<>();
+        Text newKey = new Text();
+        Text newVal = new Text();
+        newVal.set("+");
+        values.forEach((value) -> {
+           listOfConnections.add(value.toString());
+           newKey.set(key.toString()+"+"+value.toString());
+            try {
+                context.write(newKey, newVal);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        for(int i=0; i<listOfConnections.size(); i++) {
+            for (int j = i + 1; j < listOfConnections.size(); j++) {
+                String initial = listOfConnections.get(i).trim();
+                String second = listOfConnections.get(j).trim();
+                if (Integer.parseInt(initial) < Integer.parseInt(second)) {
+                    newKey.set(initial + "+" + second);
+                    newVal.set("-");
+                }
+                else {
+                    newKey.set(second + "+" + initial);
+                    newVal.set("-");
+                }
+                context.write(newKey, newVal);
+            }
+        }
+    }
+}
